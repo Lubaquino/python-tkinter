b084a62a4515197f2946208b445f10ad7850cbd3
Chad Bartel <chadbartel@users.noreply.github.com>
2015-12-14 13:58:54 -0800

    Update rngesus.py

--- rngesus.py
+++ rngesus.py
@@ -14,22 +14,25 @@ class Application(ttk.Frame):
     # master=None means this is the parent object
     def __init__(self, master=None):
         ttk.Frame.__init__(self, master)
-        self.numText = ttk.StringVar()
-        self.numText.set("0")
         self.pack()
         self.createWidgets()
 
     # We want buttons and text box, so we need a .createWidgets function
     def createWidgets(self):
+        # Declare our string variable to be connected to our label
+        self.numText = ttk.StringVar()
+        # Give our string var a default value when initialized
+        self.numText.set("0")
         # Create button to generate random number
         self.numberGen = ttk.Button(self)
         # Overlay some text over the number generator button
         self.numberGen["text"] = "Generate a number"
         # Define what numberGen will do when clicked
         self.numberGen["command"] = self.generateNum
-        self.numberGen.bind('<ButtonRelease-1>', self.updateText)
         # Create label to display number
-        self.numberLabel = ttk.Label(self, textvariable=self.numText)
+        self.numberLabel = ttk.Label(self)
+        # Hook our label up to our string variable
+        self.numberLabel["textvariable"] = self.numText
         # Create a quit button that closes the window, make the text red
         self.QUIT = ttk.Button(self, text="Quit", command=root.destroy)
 
@@ -41,12 +44,7 @@ class Application(ttk.Frame):
 
     # Create the number generate function
     def generateNum(self):
-        self.numText = str(randint(1, 10))
-        return self.numText
-
-    # Create text update event function
-    def updateText(self, event):
-        self.numberLabel = self.numText
+        self.numText.set(str(randint(1, 10)))
 
 root = ttk.Tk()
 
