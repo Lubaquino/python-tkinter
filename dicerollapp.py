e78a9d2b214316041d5c0617d5608a3ce159714f
Chad Bartel <chadbartel@users.noreply.github.com>
2015-12-16 08:27:51 -0800

    Update dicerollapp.py

--- dicerollapp.py
+++ dicerollapp.py
@@ -15,28 +15,33 @@ class Application(tk.Frame):
 
 # create widgets in separate function
     def createWidgets(self):
-        # TODO: create one button to roll the dice
+        # create one button to roll the dice
         self.rollDice = tk.Button(self)
         self.rollDice["text"] = "Roll!"
-        # TODO: create frame to house text output
+
+        # create frame to house text output
         self.outputFrame = tk.Frame(self)
-        # TODO: create 4 labels
+
+        # create 4 labels
         self.rollLabel = tk.Label(self, text="Roll ")
         self.dLabel = tk.Label(self, text=" d ")
         self.timesLabel = tk.Label(self, text=" times.")
         self.resultsVar = tk.StringVar()
         self.resultsLable = tk.Label(self, textvariable=self.resultsVar)
-        # TODO: create 2 combo boxes
+
+        # create 2 combo boxes
         self.numDiceVar = tk.StringVar()
         self.numDiceCombo = ttk.Combobox(self, state="readonly",
                                          justify="center",
                                          textvariable=self.numDiceVar)
         self.numDiceCombo["values"] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        self.numDiceCombo.current(0)
         self.dieSizeVar = tk.StringVar()
         self.dieSizeCombo = ttk.Combobox(self, state="readonly",
                                          justify="center",
                                          textvariable=self.dieSizeVar)
         self.dieSizeCombo["values"] = (4, 6, 8, 10, 12, 20, 100)
+        self.dieSizeCombo.current(5)
 
         # TODO: arrange each widget in the window
 
@@ -52,16 +57,15 @@ class Application(tk.Frame):
         for i in diceNum:
             rollDict[i] = self.rollDie(dieSize)
             rollTotal += rollDict[i]
-        return self.dictToString(rollDict), rollTotal
+        return self.dictToString(rollDict) + "\nTotal = " + rollTotal
 
     def dictToString(self, dict):
         results = ""
-        results.join('{}{}'.format(k, v) for k, v in sorted(dict.items()))
+        results.join('Roll {}: {}\n'.format(k, v) for k, v in sorted(dict.items()))
         return results
 
-
-# TODO: create data validation for the entry widget
-
-# TODO: error handlers
-
 # TODO: specify size of window, create instance, launch window
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(root)
+    root.mainloop()
