4a1fcc6f96312a2d36642ecb9fbc259960391eb3
Chad Bartel <chadbartel@users.noreply.github.com>
2015-12-16 17:18:00 -0800

    Update dicerollapp.py

--- dicerollapp.py
+++ dicerollapp.py
@@ -16,8 +16,10 @@ class Application(Frame):
 # create widgets in separate function
     def createWidgets(self):
 
-        # create frame to house label and combobox
-        self.labelComboFrame = Frame(self)
+        # create 3 frames
+        self.buttonFrameFrame = Frame(self)
+        self.labelComboFrame = Frame(self.buttonFrameFrame)
+        self.resultsFrame = Frame(self)
 
         # create 3 labels
         self.rollLabel = Label(self.labelComboFrame,
@@ -25,44 +27,44 @@ class Application(Frame):
         self.dLabel = Label(self.labelComboFrame,
                             text=" d ")
         self.resultsVar = StringVar()
-        self.resultsLabel = Label(self,
+        self.resultsLabel = Label(self.labelComboFrame,
                                   text="See results here!",
+                                  width=10,
+                                  height=5,
                                   textvariable=self.resultsVar)
 
         # create 2 combo boxes
         self.numDiceVar = IntVar()
-        self.numDiceVar.set(0)
         self.numDiceCombo = ttk.Combobox(self.labelComboFrame,
                                          state="readonly",
                                          justify="center",
                                          width=2,
                                          textvariable=self.numDiceVar)
         self.numDiceCombo["values"] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-        self.numDiceCombo.current(0)
         self.dieSizeVar = IntVar()
-        self.dieSizeVar.set(0)
         self.dieSizeCombo = ttk.Combobox(self.labelComboFrame,
                                          state="readonly",
                                          justify="center",
                                          width=2,
                                          textvariable=self.dieSizeVar)
         self.dieSizeCombo["values"] = (4, 6, 8, 10, 12, 20, 100)
-        self.dieSizeCombo.current(5)
 
         # create one button to roll the dice
-        self.rollButton = Button(self,
+        self.rollButton = Button(self.buttonFrameFrame,
                                  text="Roll!",
                                  command=self.diceRolls(self.dieSizeVar.get(),
                                                         self.numDiceVar.get()))
 
-        # TODO: arrange each widget in the window
+        # arrange each widget in the window
         self.rollLabel.pack(side="left")
         self.numDiceCombo.pack(side="left")
         self.dLabel.pack(side="left")
         self.dieSizeCombo.pack(side="left")
-        self.labelComboFrame.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
-        self.rollButton.grid(row=2, column=1, columnspan=2, pady=5)
-        self.resultsLabel.grid(row=0, column=4, rowspan=3)
+        self.labelComboFrame.pack(side="top")
+        self.rollButton.pack(fill="both")
+        self.buttonFrameFrame.pack(side="left")
+        self.resultsLabel.pack()
+        self.resultsFrame.pack(side="left")
 
 # create dice rolling function
     def rollDie(self, dieSize):
@@ -86,5 +88,5 @@ class Application(Frame):
 # create instance, launch window
 if __name__ == '__main__':
     root = Tk()
-    app = Application(root)
+    app = Application(master=root)
     root.mainloop()
