2c8bbe115d5280412b2d01616efb9093ffd5a2e7
Chad Bartel <chadbartel@users.noreply.github.com>
2015-12-21 14:14:24 -0800

    Update dicerollapp.py

--- dicerollapp.py
+++ dicerollapp.py
@@ -25,14 +25,6 @@ class Application(Frame):
                                text="Roll ")
         self.dLabel = Label(self.labelComboFrame,
                             text=" d ")
-        self.resultsVar = StringVar()
-        self.resultsVar.set("Roll the dice!")
-        self.resultsLabel = Label(self,
-                                  width=11,
-                                  height=5,
-                                  relief=SUNKEN,
-                                  anchor=NW,
-                                  textvariable=self.resultsVar)
 
         # create 2 combo boxes
         self.numDiceVar = IntVar()
@@ -41,7 +33,7 @@ class Application(Frame):
                                          justify="center",
                                          width=2,
                                          textvariable=self.numDiceVar)
-        self.numDiceCombo["values"] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        self.numDiceCombo["values"] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
         self.numDiceCombo.current(0)
         self.dieSizeVar = IntVar()
         self.dieSizeCombo = ttk.Combobox(self.labelComboFrame,
@@ -60,6 +52,16 @@ class Application(Frame):
         # Create a quit button that closes the window
         self.QUIT = Button(self.buttonFrameFrame, text="Quit", command=root.destroy)
 
+        # Create listbox for each roll
+        self.resultsList = Listbox(self)
+
+        # Create scrollbar widget for new text field
+        self.scrollbar = Scrollbar(self)
+
+        # attach listbox to scrollbar
+        self.resultsList.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.resultsList.yview)
+
         # arrange each widget in the window
         self.rollLabel.pack(side="left")
         self.numDiceCombo.pack(side="left")
@@ -69,7 +71,8 @@ class Application(Frame):
         self.rollButton.pack(fill="both", padx=3, pady=3)
         self.QUIT.pack(fill="both", padx=3, pady=3)
         self.buttonFrameFrame.pack(side="left")
-        self.resultsLabel.pack(side="left", padx=5, pady=5)
+        self.resultsList.pack(side="left", padx=5, pady=5)
+        self.scrollbar.pack(side="left", fill=Y)
 
     # create dice rolling function
     def rollDie(self, dieSize):
@@ -77,13 +80,13 @@ class Application(Frame):
         return randint(1, dieSize)
 
     def diceRolls(self):
+        # clear previous values from rollList
+        self.resultsList.delete(0, END)
         # roll 'diceNum' dice of size 'dieSize'
         dieSize = int(self.dieSizeCombo.get())
         diceNum = int(self.numDiceCombo.get())
-        rollDict = {}
         for i in range(diceNum):
-            rollDict[i+1] = self.rollDie(dieSize)
-        self.resultsVar.set(str(rollDict))
+            self.resultsList.insert(END, self.rollDie(dieSize))
 
 # create instance, launch window
 if __name__ == '__main__':
