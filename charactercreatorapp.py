6db2eea2c59a73798ff971921d5fd6cd27bb920a
Chad Bartel <chadbartel@users.noreply.github.com>
2016-01-05 13:45:52 -0800

    Update charactercreatorapp.py

--- charactercreatorapp.py
+++ charactercreatorapp.py
@@ -32,10 +32,10 @@ class Application(Frame):
                                 text="3 of 4d6 x 6")
         self.roll3Label = Label(self.radioDiceFrame,
                                 text="3d6 x 6")
-        self.statsLabel = Label(self.statsFrame,
-                                text="Stats")
+        self.abilitiesLabel = Label(self.statsFrame,
+                                text="Abilities")
         self.raceBonusLabel = Label(self.statsFrame,
-                                    text="Racial Bonus")
+                                    text="Racial\nBonus")
         self.strLabel = Label(self.statsFrame,
                               text="Str")
         self.dexLabel = Label(self.statsFrame,
@@ -50,56 +50,65 @@ class Application(Frame):
                               text="Cha")
         self.strBonusLabelVar = StringVar()
         self.strBonusLabel = Label(self.statsFrame,
-                                   width=2,
                                    textvariable=self.strBonusLabelVar)
         self.dexBonusLabelVar = StringVar()
         self.dexBonusLabel = Label(self.statsFrame,
-                                   width=2,
                                    textvariable=self.dexBonusLabelVar)
         self.conBonusLabelVar = StringVar()
         self.conBonusLabel = Label(self.statsFrame,
-                                   width=2,
                                    textvariable=self.conBonusLabelVar)
         self.intBonusLabelVar = StringVar()
         self.intBonusLabel = Label(self.statsFrame,
-                                   width=2,
                                    textvariable=self.intBonusLabelVar)
         self.wisBonusLabelVar = StringVar()
         self.wisBonusLabel = Label(self.statsFrame,
-                                   width=2,
                                    textvariable=self.wisBonusLabelVar)
         self.chaBonusLabelVar = StringVar()
         self.chaBonusLabel = Label(self.statsFrame,
-                                   width=2,
                                    textvariable=self.chaBonusLabelVar)
 
         # TODO: create 6 entry fields
         self.strEntryVar = IntVar()
         self.strEntry = Entry(self.statsFrame,
+                              width=2,
                               textvariable=self.strEntryVar)
         self.dexEntryVar = IntVar()
         self.dexEntry = Entry(self.statsFrame,
+                              width=2,
                               textvariable=self.dexEntryVar)
         self.conEntryVar = IntVar()
         self.conEntry = Entry(self.statsFrame,
+                              width=2,
                               textvariable=self.conEntryVar)
         self.intEntryVar = IntVar()
         self.intEntry = Entry(self.statsFrame,
+                              width=2,
                               textvariable=self.intEntryVar)
         self.wisEntryVar = IntVar()
         self.wisEntry = Entry(self.statsFrame,
+                              width=2,
                               textvariable=self.wisEntryVar)
         self.chaEntryVar = IntVar()
         self.chaEntry = Entry(self.statsFrame,
+                              width=2,
                               textvariable=self.chaEntryVar)
 
         # TODO: create 2 drop-downs
         self.raceDropMenuVar = StringVar()
         self.raceDropMenu = ttk.Combobox(self.raceClassFrame,
+                                         state="readonly",
                                          textvariable=self.raceDropMenuVar)
+        self.raceDropMenu["values"] = ('Human', 'Dwarf', 'Elf', 'Gnome',
+                                       'Half-Elf', 'Half-Orc', 'Halfling')
+        self.raceDropMenu.current(6)
         self.classDropMenuVar = StringVar()
         self.classDropMenu = ttk.Combobox(self.raceClassFrame,
+                                          state="readonly",
                                           textvariable=self.classDropMenuVar)
+        self.classDropMenu["values"] = ('Barbarian', 'Bard', 'Clerid', 'Druid',
+                                        'Fighter', 'Monk', 'Paladin', 'Ranger',
+                                        'Rogue', 'Sorcerer', 'Wizard')
+        self.classDropMenu.current(8)
 
         # TODO: create 3 buttons
         self.createButton = Button(self.buttonFrame,
@@ -141,8 +150,8 @@ class Application(Frame):
         self.saveButton.grid(row=1, column=0, sticky=W+E)
         self.quitButton.grid(row=2, column=0, sticky=W+E)
 
-        self.statsFrame.grid(row=0, column=1)
-        self.statsLabel.grid(row=0, column=0, columnspan=2)
+        self.statsFrame.grid(row=0, column=1, rowspan=3)
+        self.abilitiesLabel.grid(row=0, column=0, columnspan=2)
         self.raceBonusLabel.grid(row=0, column=2)
         self.strLabel.grid(row=1, column=0)
         self.strEntry.grid(row=1, column=1)
