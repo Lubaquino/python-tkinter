8f8c8b20f951f008625d050cc6fa7cc8524a36fa
Chad Bartel <chadbartel@users.noreply.github.com>
2016-03-28 18:36:50 -0700

    Create whatDoIDo.py

---
+++ whatDoIDo.py
@@ -0,0 +1,8 @@
+from random import randint, choice
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+root = Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
