9c5751f23cf4f95517dbad7f14796839f710aeaf
Chad Bartel <chadbartel@users.noreply.github.com>
2016-03-29 09:16:02 -0700

    Update whatDoIDo.py

--- whatDoIDo.py
+++ whatDoIDo.py
@@ -3,18 +3,43 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
-# TODO: Create frame for widgets
+class Application(Frame):
 
-# TODO: Create label for "flavor"
+    # Initialize app
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-# TODO: Create dynamic label widget to display result
+    # TODO: Build method to house all app widgets
+    def createWidgets(self):
 
-# TODO: Create button to generate result
+        # TODO: Create frame for widgets
+        self.whatDoFrame = Frame(self)
 
-# TODO: Create file bar to load a text file to use as results list and quit
+        # TODO: Create label for "flavor" above dynmic label
+        self.flavorLabel = Label(self.whatDoFrame,
+                                 text="What about...")
 
-# TODO: Load text file for result choices and close text file
+        # TODO: Create dynamic label widget to display result
+        self.resultText = StringVar()
+        self.resultLabel = Label(self.whatDoFrame,
+                                 relief="sunken",
+                                 justify=CENTER,
+                                 textvariable=self.resultText)
 
-root = Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
+        # TODO: Create button to generate result
+
+
+    # TODO: Create file bar to load a text file to use as results list and quit
+
+
+    # TODO: Load text file for result choices and close text file
+
+
+# Run app
+if __name__ == '__main__':
+    root = Tk()
+    app = Application(master=root)
+    app.master.title("What Do I Do?!")
+    root.mainloop()
