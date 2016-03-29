ab1bbc765a687517f9e3a62acc71b601a4eb5ff8
Chad Bartel <chadbartel@users.noreply.github.com>
2016-03-28 18:45:42 -0700

    Update whatDoIDo.py

--- whatDoIDo.py
+++ whatDoIDo.py
@@ -3,6 +3,18 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
+# TODO: Create frame for widgets
+
+# TODO: Create label for "flavor"
+
+# TODO: Create dynamic label widget to display result
+
+# TODO: Create button to generate result
+
+# TODO: Create file bar to load a text file to use as results list and quit
+
+# TODO: Load text file for result choices and close text file
+
 root = Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()
