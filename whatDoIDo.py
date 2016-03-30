883d0b574d9568bae4fe8e7714da8aa0941f1d85
Chad Bartel <chadbartel@users.noreply.github.com>
2016-03-29 18:59:30 -0700

    Update whatDoIDo.py

--- whatDoIDo.py
+++ whatDoIDo.py
@@ -14,18 +14,24 @@ class Application(Frame):
     # TODO: Build method to house all app widgets
     def createWidgets(self):
 
-        # Create frame for widgets
+        # Create frames for widgets
         self.whatDoFrame = Frame(self)
+        self.textFrame = Frame(self)
 
         # TODO: Create 'File' and 'Quit' on menubar
         self.menubar = Menu(root)
+
         self.filemenu = Menu(self.menubar,
                              tearoff=0)
-        self.filemenu.add_command(label="Open",
-                                  command=self.openFile)
+        self.filemenu.add_command(label="Save",
+                                  command=self.saveFile)
+        self.filemenu.add_command(label="Load",
+                                  command=self.loadFile)
         self.filemenu.add_separator()
         self.filemenu.add_command(label="Quit",
                                   command=root.quit)
+        self.menubar.add_cascade(label="File",
+                                 menu=self.filemenu)
         root.config(menu=self.menubar)
 
         # TODO: Create label for "flavor" above dynamic label
@@ -37,6 +43,7 @@ class Application(Frame):
         self.resultLabel = Label(self.whatDoFrame,
                                  relief="sunken",
                                  justify=CENTER,
+                                 width=15,
                                  textvariable=self.resultText)
 
         # TODO: Create button to generate result
@@ -44,14 +51,33 @@ class Application(Frame):
                                    text="I'm Bored!",
                                    command=self.chooseResult)
 
+        # TODO: Create text widget for displaying/creating text files
+        self.textBox = Text(self.textFrame,
+                            wrap=WORD,
+                            width=15,
+                            height=10)
+
+        # TODO: Create Y-scrollbar for text widget
+        self.scrollY = Scrollbar(self.textFrame)
+        self.textBox.config(yscrollcommand=self.scrollY.set)
+        self.scrollY.config(command=self.textBox.yview)
+
         # TODO: Arrange widgets in frame
         self.whatDoFrame.grid(row=0, column=0)
         self.flavorLabel.grid(row=0, column=0, sticky=W+E)
         self.resultLabel.grid(row=1, column=0, sticky=W+E)
         self.resultButton.grid(row=2, column=0)
 
+        self.textFrame.grid(row=0, column=1)
+        self.textBox.grid(row=0, column=0)
+        self.scrollY.grid(row=0,column=1, sticky=N+S)
+
     # TODO: Load text file for result choices and close text file
-    def openFile(self):
+    def loadFile(self):
+        pass
+
+    # TODO: Save text file for results choices and close text file
+    def saveFile(self):
         pass
 
     # TODO: Build method to choose a result from the text file
