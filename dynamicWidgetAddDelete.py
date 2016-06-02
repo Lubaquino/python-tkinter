e23750197b9f7cb46cec943c17b053b9d83ef77e
Chad Bartel <chadbartel@users.noreply.github.com>
2016-06-02 11:30:04 -0700

    Update dynamicWidgetAddDelete.py
    Found from: http://stackoverflow.com/a/14804825

--- dynamicWidgetAddDelete.py
+++ dynamicWidgetAddDelete.py
@@ -3,10 +3,13 @@ import tkinter as tk
 root = tk.Tk()
 
 def add_new():
-    b = tk.Button(root,text="Click to destroy")
+    b = tk.Button(root, text="Click to destroy")
     b.pack()
-    b.config(command=b.pack_forget)
+    # pack_forget doesn't destroy the widget, it saves it for later use/repacking
+    # b.config(command=b.pack_forget)
+    # destroy will destroy the widget and remove it from memory, preventing memory leak
+    b.config(command=b.destroy)
 
-b = tk.Button(root,text="Add_new",command=add_new)
+b = tk.Button(root, text="Add_new", command=add_new)
 b.pack()
 root.mainloop()
