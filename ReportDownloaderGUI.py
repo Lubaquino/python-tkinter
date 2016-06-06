229e298e4e25a4a6c26740bf7bcef87e8fb21910
Chad Bartel <chadbartel@users.noreply.github.com>
2016-06-06 14:00:31 -0700

    Update ReportDownloaderGUI.py

--- ReportDownloaderGUI.py
+++ ReportDownloaderGUI.py
@@ -12,35 +12,35 @@ PAUSE = 2.5
 
 class Application(Frame):
 
-# initialize app
+    # initialize app
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.reportEmailDict = {}
         self.pack()
         self.createWidgets()
 
-# create the widgets
+    # create the widgets
     def createWidgets(self):
-# add frames
+        # add frames
         self.masterFrame = Frame(self)
         self.masterFrame.grid(row=0, column=0)
-# frame for full file path of all screenshots and appending date on file save
+        # frame for full file path of all screenshots and appending date on file save
         self.frame1 = Frame(self.masterFrame)
         self.frame1.grid(row=0, column=0)
-# frame for live text widget
+        # frame for live text widget
         self.frame5 = Frame(self.masterFrame)
         self.frame5.grid(row=1, column=0)
-# frame for label and add report button
+        # frame for label and add report button
         self.frame2 = Frame(self.masterFrame)
         self.frame2.grid(row=2, column=0)
-# frame to contain run button below attachment lines
+        # frame to contain run button below attachment lines
         self.frame4 = Frame(self.masterFrame)
         self.frame4.grid(row=2, column=1)
-# frame to house all attachment lines
+        # frame to house all attachment lines
         self.frame3 = Frame(self.masterFrame)
         self.frame3.grid(row=3, column=0)
 
-# add labels
+        # add labels
         self.screenshotLabel = Label(self.frame1,
                                      text="Path to screenshots: ")
         self.screenshotLabel.grid(row=0, column=0)
@@ -48,13 +48,14 @@ class Application(Frame):
                                      text="Include date on filename(s)? ")
         self.appendDateLabel.grid(row=0, column=0)
 
-# add screenshot path entry widget
-        self.screenshotEntry = Entry(self.frame1)
-        self.screenshotEntry.delete(0, END)
-        self.screenshotEntry.insert(0, "C:\\docs\\pics\\")
+        # add screenshot path entry widget
+        self.screenshotEntryVar = StringVar()
+        self.screenshotEntry = Entry(self.frame1,
+                                     textvariable=self.screenshotEntryVar)
+        self.screenshotEntryVar.set("C:\\docs\\pics\\")
         self.screenshotEntry.grid(row=0, column=1)
 
-# text widget to display live updates
+        # text widget to display live updates
         self.liveText = Text(self.frame5,
                              wrap=WORD,
                              width=40,
@@ -63,82 +64,84 @@ class Application(Frame):
                              "This is where you'll see live updates from the program as it runs.\n")
         self.liveText.grid(row=0, column=0)
 
-# add buttons
-# add button to create attachment lines
+        # add buttons
+        # add button to create attachment lines
         self.addReportButton = Button(self.frame2,
                                       text="Add Report",
                                       command=self.addReportLine)
         self.addReportButton.grid(row=0, column=1)
-# create button to run the search and download on what is in the entry fields
+        # create button to run the search and download on what is in the entry fields
         self.runButton = Button(self.frame4,
                                 text="Run",
-                                command=self.findAndSaveAttachment)
+                                command=self.createDictionary)
         self.runButton.grid(row=0, column=2)
 
-# add new line to window to search and dl another file
+    # add new line to window to search and dl another file
     def addReportLine(self):
-# create frame to house the newly added widgets
+        # create frame to house the newly added widgets
         self.newReportFrame = Frame(self.frame3)
         self.newReportFrame.pack()
 
-# create label to indicate what entry widget is used for
+        # create label to indicate what entry widget is used for
         self.label1 = Label(self.newReportFrame,
                             text="Email subject screenshot: ")
         self.label1.grid(row=0, column=0)
 
-# create entry widget to take user input for filename and extension
-        self.entry1 = Entry(self.newReportFrame)
-# clear any contents that might be in the entry widget and put placeholder text
-        self.entry1.delete(0, END)
-        self.entry1.insert(0, "Enter screenshot filename and extension here")
+        # create entry widget to take user input for filename and extension
+        self.entry1Var = StringVar()
+        self.entry1 = Entry(self.newReportFrame,
+                            textvariable=self.entry1Var)
+        self.entry1Var.set("Enter screenshot filename and extension here")
         self.entry1.grid(row=0, column=1)
 
-# create entry widget to take user input for full save path and new file name
+        # create entry widget to take user input for full save path and new file name
         self.label2 = Label(self.newReportFrame,
                             text="Report save path: ")
         self.label2.grid(row=0, column=2)
-        self.entry2 = Entry(self.newReportFrame)
-# clear any contents that might be in the entry widget and put placeholder text
-        self.entry2.delete(0, END)
-        self.entry2.insert(0, "Enter full save path and new file name")
+
+        # create entry widget to take save path and new file name
+        self.entry2Var = StringVar()
+        self.entry2 = Entry(self.newReportFrame,
+                            textvariable=self.entry2Var)
+        self.entry2Var.set("Enter full save path and new file name")
         self.entry2.grid(row=0, column=3)
 
-# create button that deletes all widgets in new frame and self
+        # create button that deletes all widgets in new frame and self
         self.dButton = Button(self.newReportFrame,
                               text="Click to delete this line")
         self.dButton.config(command=self.newReportFrame.destroy)
         self.dButton.grid(row=0, column=4)
 
+    def createDictionary(self):
+        # create dictionary containing key-value pairs
+            # key   = filename of picture + extension
+            # value = full save path + save as name
+        return None
+
+    def saveProfile(self):
+        # save all settings, filenames, file paths, etc to a text file
+        return None
+
+    def loadProfile(self):
+        # load all settings, filenames, file paths, etc from a text file
+        return None
+
     def findAndSaveAttachment(self, k, v):
-# find and save email attachment using key-value pair
+        # find and save email attachment using key-value pair
         self.findAttachment(k, v)
         self.saveAttachment(k, v)
 
     def findAttachment(self, k, v):
-# find the attachment using key-value pair
+        # find the attachment using key-value pair
         return k, v
 
     def saveAttachment(self, k, v):
-# save the attachment using key-value pair
+        # save the attachment using key-value pair
         return k, v
 
     def changeFileName(self, k, v):
-# change filename of attachment before saving using key-value pair
+        # change filename of attachment before saving using key-value pair
         return k, v
-
-    def createDictionary(self):
-# create dictionary containing key-value pairs
-    # key   = filename of picture + extension
-    # value = full save path + save as name
-        return None
-
-    def saveProfile(self):
-# save all settings, filenames, file paths, etc to a text file
-        return None
-
-    def loadProfile(self):
-# load all settings, filenames, file paths, etc from a text file
-        return None
 
 if __name__ == '__main__':
     root = Tk()
